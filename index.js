038e6bd081ee884d5a7154edb0a5d8b745da73ea
zuulaxx89 <guigui.8929@gmail.com>
2022-04-15 16:47:48 +0200

    Init

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require('socket.io');
+const io = new Server(server);
+
+app.use(express.static('public'));
+
+io.on('connection', (socket) => {
+  socket.broadcast.emit('chat message', {
+    message: "Un utilisateur s'est connecté !",
+  });
+  socket.on('disconnect', () => {
+    socket.broadcast.emit('chat message', {
+      message: "Un utilisateur s'est déconnecté !",
+    });
+  });
+});
+
+io.on('connection', (socket) => {
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
+});
+
+server.listen(3000, () => {
+  console.log('listening on *:3000');
+});
