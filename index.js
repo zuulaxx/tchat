b2794c2f051dd33a6707f02371540dea1b7fbc4f
zuulaxx89 <guigui.8929@gmail.com>
2022-05-05 16:02:15 +0200

    Update

--- index.js
+++ index.js
@@ -7,32 +7,31 @@ const io = new Server(server);
 
 app.use(express.static('public'));
 
-// var users = [];
-var users = 0;
+var users = {};
 
 io.on('connection', (socket) => {
-  socket.broadcast.emit('chat message', {
-    message: "Un utilisateur s'est connecté !",
-    type: 'system',
-  });
-  users++;
-  console.log(users + ' utilisateurs');
+  users[socket.id] = socket.handshake.query.username;
+
   socket.on('disconnect', () => {
+    delete users[socket.id];
     socket.broadcast.emit('chat message', {
-      message: "Un utilisateur s'est déconnecté !",
+      message: `<b>${users[socket.id]}</b> s'est déconnecté !`,
       type: 'system',
     });
-    users--;
-    console.log(users + ' utilisateurs');
   });
-});
 
-io.on('connection', (socket) => {
   socket.on('chat message', (msg) => {
     io.emit('chat message', msg);
+  });
+
+  socket.broadcast.emit('chat message', {
+    message: `<b>${users[socket.id]}</b> s'est connecté !`,
+    type: 'system',
   });
 });
 
 server.listen(3000, () => {
   console.log('listening on *:3000');
 });
+
+// TODO : Rajouter le thème sombre et le nombre d'utilisateurs connectés actuellement.
---
+++ public/app.js
@@ -0,0 +1,31 @@
+const messages = document.getElementById('messages');
+const form = document.getElementById('form');
+const input = document.getElementById('input');
+
+// TODO : faire un bouton pour changer le nom d'utilisateur et dire si il est pris ou non.
+var username = localStorage.getItem('username');
+if (username == null) {
+  username = prompt("Quel est ton nom d'utilisateur ?");
+  localStorage.setItem('username', username);
+}
+
+const socket = io({ query: { username: username } });
+
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+  if (input.value) {
+    socket.emit('chat message', { message: input.value, user: username });
+    input.value = '';
+  }
+});
+
+socket.on('chat message', function (msg) {
+  const item = document.createElement('li');
+  item.textContent = msg.message;
+
+  if (msg.type == 'system') item.classList.add('system-message');
+  else item.innerHTML = '<b>' + msg.user + ' :</b> ' + item.textContent;
+
+  messages.appendChild(item);
+  window.scrollTo(0, document.body.scrollHeight);
+});
