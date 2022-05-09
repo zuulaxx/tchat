4af0b6ae3af20507a3be8b4585604b288f5e4433
zuulaxx89 <guigui.8929@gmail.com>
2022-05-09 11:06:23 +0200

    Correction bugs

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.static('public'));
 var users = {};
 function userExists(username) {
   var duhArray = [];
-  for (const user of users) {
+  for (const user in users) {
     if (user == username) duhArray.push(user);
   }
   return duhArray;
